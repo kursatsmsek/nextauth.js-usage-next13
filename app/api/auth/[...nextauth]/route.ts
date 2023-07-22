3ff48bc0cf792b8a5b50319ad736d6f997f3b372
kursatsmsek <kursatsimsek@protonmail.ch>
2023-07-22 17:19:22 +0300

    credentials provider added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { redirect } from "next/dist/server/api-utils"
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   throw new Error("Google client ID and client secret are required.")
@@ -7,6 +9,23 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 
 const handler = NextAuth({
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: "1",
+          name: "developed by Kursat Simsek",
+          email: "kursatsimsek@protonmail.ch",
+        }
+
+        if (user) return user
+        else return null
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
